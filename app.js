44d03c52afbdcc4480bf9813da2debd704a2e9d9
JuvitaSaini09 <juvitasaini@gmail.com>
2021-08-28 07:33:44 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+var input = document.querySelector(".inputText");
+var btn_translate = document.querySelector(".translate-btn");
+var output = document.querySelector(".outputText");
+
+function getText(inputt) {
+    var text = "https://api.funtranslations.com/translate/minion.json" + "?" + "text=" + inputt;
+    return text;
+
+}
+
+function errorHandler(error) {
+    console.log("error !!", error);
+}
+
+function Handler() {
+    fetch(getText(input.value))
+        .then(res => res.json())
+        .then(json => {
+            var banana_translation = json.contents.translated;
+            output.innerText = banana_translation;
+
+        })
+        .catch(errorHandler);
+}
+
+btn_translate.addEventListener("click", Handler)
